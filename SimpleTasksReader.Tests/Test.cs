68580446b4aaf08e29226b95f7686a1a0c983250
michal_urbanski <michal_urbanski@windowslive.com>
2016-05-09 21:29:59 +0000

    Changes nunit to 2.6.4 version

---
+++ SimpleTasksReader.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace SimpleTasksReader.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void Test_NUnit()
+        {
+            Assert.True(true); 
+        }
+    }
+}
