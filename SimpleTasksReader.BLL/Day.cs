51ae8a6a6c5013821d3528084bcf8c7ef7ce3bfb
michal_urbanski <michal_urbanski@windowslive.com>
2016-05-22 19:00:06 +0000

    Adds metadata for day class

--- SimpleTasksReader.BLL/Day.cs
+++ SimpleTasksReader.BLL/Day.cs
@@ -1,20 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace SimpleTasksReader.BLL
 {
+    /// <summary>
+    /// Represents day. Day has name, date and list of actions.
+    /// </summary>
     public class Day
     {
-        public List<CustomAction> CustomActions { get; }
+        private string _dayEntry;
 
-        public Day()
+        public List<CustomAction> CustomActions { get; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        private Day()
         {
             CustomActions = new List<CustomAction>(); 
+        }
+
+        public Day(string dayEntry) : this()
+        {
+            _dayEntry = dayEntry;
+
+            ParseMetadata(_dayEntry);
         }
 
         public void AddAction(CustomAction customAction)
         {
             CustomActions.Add(customAction);
+        }
+
+        private void ParseMetadata(string entry)
+        {
+            var splitted = entry.Split();
+
+            Name = splitted[0];
+            Date = DateTime.Parse(splitted[1], CultureInfo.InvariantCulture);
         }
     }
 }
--- SimpleTasksReader.Tests/Models/DayTests.cs
+++ SimpleTasksReader.Tests/Models/DayTests.cs
@@ -1,5 +1,6 @@
-﻿using Xunit; 
+﻿using Xunit;
 using SimpleTasksReader.BLL;
+using System;
 
 namespace SimpleTasksReader.Tests.Models
 {
@@ -8,11 +9,21 @@ namespace SimpleTasksReader.Tests.Models
         [Fact]
         public void Test_Add_Action_To_Day()
         {
-            Day day = new Day();
+            Day day = new Day("Monday 2016-05-01");
             CustomAction customAction = new CustomAction();
             day.AddAction(customAction);
 
             Assert.Equal<int>(1, day.CustomActions.Count);
+        }
+
+        [Fact]
+        public void Test_Day_Has_Name_And_Date()
+        {
+            string dayEntry = "Monday 2016-05-01";
+            Day day = new Day(dayEntry);
+
+            Assert.Equal("Monday", day.Name);
+            Assert.Equal(new DateTime(2016, 5, 1), day.Date);
         }
     }
 }
